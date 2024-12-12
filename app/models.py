af67df17796a8d7f07698a586147df13793d3063
abc1235775 <abc1235775@gmail.com>
2024-12-12 14:54:25 +0800

    splite main.py

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column,Date,Integer,String,Boolean
+from .database import Base
+#Define Model
+class Todo(Base):
+    __tablename__= "Todos"
+    id = Column(Integer,primary_key=True,index=True)
+    title = Column(String(100),nullable=False)
+    description = Column(String,nullable=True)
+    completed = Column(Boolean,default=False)
+    due_date = Column (Date,nullable=True)
